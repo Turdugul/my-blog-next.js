97a6014a3ef7286348825609f55eb330faf14505
Turdugul <okonbaevaturdugul@gmail.com>
2024-12-04 23:16:38 +0000

    installed, add header and dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from 'next-themes';
+import ThemeCom from './components/ThemeCom';
+import Header from "./components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,11 +22,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          <ThemeCom>
+            <Header />
+            {children}
+          </ThemeCom>
+        </ThemeProvider>
       </body>
     </html>
   );
