c58123ed0582c034e585e135d8a1119bcec29e43
Turdugul <okonbaevaturdugul@gmail.com>
2024-12-06 23:24:51 +0000

    mongo db user data

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,25 +1,25 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 export async function POST(req) {
-
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
   if (!WEBHOOK_SECRET) {
     throw new Error(
       'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
-
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
-
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400,
     });
   }
-  
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
@@ -39,15 +39,50 @@ export async function POST(req) {
     });
   }
 
-  const { id } = evt.data;
-  const eventType = evt.type;
+  const { id } = evt?.data;
+  const eventType = evt?.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
-  if (evt.type === 'user.created') {
-    console.log('userId:', evt.data.id);
+
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+      if (user && eventType === 'user.created') {
+        try {
+          await clerkClient.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+              isAdmin: user.isAdmin,
+            },
+          });
+        } catch (error) {
+          console.log('Error updating user metadata:', error);
+        }
+      }
+    } catch (error) {
+      console.log('Error creating or updating user:', error);
+      return new Response('Error occured', { status: 400 });
+    }
   }
-  if (evt.type === 'user.updated') {
-    console.log('user is updated:', evt.data.id);
+
+  if (eventType === 'user.deleted') {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log('Error deleting user:', error);
+      return new Response('Error occured', { status: 400 });
+    }
   }
+
   return new Response('', { status: 200 });
 }
